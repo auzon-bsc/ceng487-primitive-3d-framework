1527b484801008a40e7d1f4d9ee506c7815b277e
Oğuzhan Özer <oguzhanozer90@outlook.com>
2021-10-24 13:51:43 +0300

    Initial commit
    initial+ commit
    
    + AssignmentStandards.pdf
    + IntroToOpenGL repo from
    + some class methods to vec3d.py

---
+++ README.md
@@ -0,0 +1,2 @@
+# Computer Graphics
+ Python and OpenGL
---
+++ dev/vec3d.py
@@ -0,0 +1,82 @@
+# CENG 487 Assignment1 by
+# Oğuzhan Özer
+# StudentId: 260201039
+# October 2021
+
+class Vec3d:
+  def __init__(self, x, y, z):
+    self.x = x
+    self.y = y
+    self.z = z
+    self.w = 0
+  
+  def __str__(self):
+      str = "(x, y, z): (%s, %s, %s)" % (self.x, self.y, self.z)
+      return str
+
+  def add(self, other):
+    self.x += other.x
+    self.y += other.y
+    self.z += other.z
+    
+    print("\nRESULT OF ADDITION: %s" % self)
+  
+  def substract(self, vec2):
+    """Substract vec2 from vec1 (vec1 - vec2)
+
+    Args:
+        vec1 (Vec3d): Minuend
+        vec2 (Vec3d): Subtrahend
+    """    
+    vec2.negative()
+    self.add(vec2)
+    vec2.negative()
+
+    print("\nRESULT OF SUBSTRACTION: %s" % (self))
+  
+  def clone(self):
+    new_x = self.x
+    new_y = self.y
+    new_z = self.z
+    new_vec3d = Vec3d(new_x, new_y, new_z)
+
+    print("\nCLONE: %s" % (new_vec3d))
+
+    return new_vec3d
+
+  def negative(self):
+    self.x = -self.x
+    self.y = -self.y
+    self.z = -self.z
+
+    print("\nNEGATED VECTOR %s" % self)
+
+  def scale(self, factor):
+    self.x *= factor
+    self.y *= factor
+    self.z *= factor
+
+    print("\nSCALED VECTOR %s" % self)
+
+
+
+
+
+
+
+
+
+
+def main():
+  vec1 = Vec3d(1.3, 2.4, 5)
+  vec2 = Vec3d(1.7, 2.6, 15)
+  vec1.add(vec2)
+
+  vec3 = Vec3d(2.9, 1.5, 3)
+  vec4 = Vec3d(0.9, 0.5, 3)
+  vec3.substract(vec4)
+
+  vec5 = Vec3d(0.2, 0.5, 0.7)
+  vec5.scale(10)
+
+main()
