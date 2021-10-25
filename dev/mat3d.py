98619de5eb9b9d30f68e06644e625c202e6d7ad8
Oğuzhan Özer <oguzhanozer90@outlook.com>
2021-10-25 21:13:18 +0300

    Update mat3d.py
    __init__ done
    __str__ done

--- dev/mat3d.py
+++ dev/mat3d.py
@@ -2,3 +2,23 @@
 # Oğuzhan Özer
 # StudentId: 260201039
 # October 2021
+
+class Mat3d:
+  def __init__(self) -> None:
+      self.matrix = [[0, 0, 0, 0],
+                [0, 0, 0, 0],
+                [0, 0, 0, 0],
+                [0, 0, 0, 0]]
+  
+  def __str__(self) -> str:
+      s = ""
+      for row in self.matrix:
+        s += "\n| %d %d %d %d |" % (row[0], row[1], row[2], row[3])
+      return s
+      
+
+def main():
+  
+  pass
+
+main()
