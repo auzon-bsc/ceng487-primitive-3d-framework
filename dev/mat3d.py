debf7c021af264bf51296810026909e9190ab851
Oğuzhan Özer <oguzhanozer90@outlook.com>
2021-10-25 21:25:41 +0300

    fix hard coded matrix constructor

--- dev/mat3d.py
+++ dev/mat3d.py
@@ -4,16 +4,21 @@
 # October 2021
 
 class Mat3d:
-  def __init__(self) -> None:
-      self.matrix = [[0, 0, 0, 0],
-                [0, 0, 0, 0],
-                [0, 0, 0, 0],
-                [0, 0, 0, 0]]
+  def __init__(self, m, n) -> None:
+      self.matrix = []
+      for i in range(0, m):
+        row = []
+        for j in range(0, n):
+          row.append(0)
+        self.matrix.append(row)
   
   def __str__(self) -> str:
       s = ""
       for row in self.matrix:
-        s += "\n| %d %d %d %d |" % (row[0], row[1], row[2], row[3])
+        s += "\n|"
+        for number in row:
+          s += " " + str(number)
+        s += " |"
       return s
       
 
