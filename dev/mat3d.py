d8a43fdb51f40a94a0e69ed380f8ab9cc11b5452
Oğuzhan Özer <oguzhanozer90@outlook.com>
2021-10-25 22:08:19 +0300

    Update mat3d.py
    add set_num method
    add multiply method

--- dev/mat3d.py
+++ dev/mat3d.py
@@ -4,7 +4,9 @@
 # October 2021
 
 class Mat3d:
-  def __init__(self, m, n) -> None:
+  def __init__(self, m = 1, n = 1) -> None:
+      self.m = m
+      self.n = n
       self.matrix = []
       for i in range(0, m):
         row = []
@@ -16,14 +18,31 @@ class Mat3d:
       s = ""
       for row in self.matrix:
         s += "\n|"
-        for number in row:
-          s += " " + str(number)
+        for num in row:
+          s += " " + str(num)
         s += " |"
       return s
-      
+  
+  def set_num(self, num, row, col):
+    if (row - 1) > self.m or (col - 1) > self.n:
+      print("Error: Invalid row and column locations")
+    else:
+      self.matrix[row - 1][col - 1] = num
+
+  def multipy(self, other):
+    if(self.n != other.m):
+      print("Error: You cannot multiply %d column matrix with %d row matrix" % (self.n, other.m))
+    else:
+      tmp_m = self.m
+      tmp_n = other.n
+      tmp_mat = Mat3d(tmp_m, tmp_n)
+      for i in range(0, tmp_m):
+        for j in range(0, tmp_n):
+          for k in range(0, self.n):
+            tmp_mat.matrix[i][j] += self.matrix[i][k] * other.matrix[k][j]
+      return tmp_mat
 
 def main():
-  
   pass
 
 main()
