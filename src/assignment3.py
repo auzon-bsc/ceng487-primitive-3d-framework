2a76b6378e546900e22ce153a3d2876e3e1c0104
Oğuzhan Özer <oguzhanozer90@outlook.com>
2021-11-03 19:22:02 +0300

    ready for release v1.0
    cube is so slow

--- src/assignment3.py
+++ src/assignment3.py
@@ -2,47 +2,29 @@
 # Oğuzhan Özer
 # StudentId: 260201039
 # November 2021
+# Runs in Python 3.10.0
 
 from OpenGL.GL import *
 from OpenGL.GLUT import *
 from OpenGL.GLU import *
+
 import sys
 import math
-from obj3d import Obj3d
 
+from obj3d import Obj3d
 from vec3d import Vec3d
 
 # Number of the glut window.
 window = 0
 
-# Rotation angle for the triangle.
+# Rotation angle for the objects.
 r_angle = 0.0
 
 # Subdivision number
 sd = 10
 
-object_to_draw = "NONE"
-
-def generate_sphere_vertices(radius, sector_count, stack_count):
-	ver_arr2d = []
-	for stack_step in range(stack_count + 1):
-		tmp_arr = []
-		for sector_step in range(sector_count + 1):
-			theta = (2 * math.pi) * (sector_step / sector_count)
-			phi = (math.pi / 2) - (math.pi * stack_step / stack_count)
-
-			x = radius * math.cos(phi) * math.cos(theta)
-			y = radius * math.cos(phi) * math.sin(theta)
-			z = radius * math.sin(phi)
-
-			tmp_vec = Vec3d(x, y, z)
-			tmp_arr.append(tmp_vec)
-		ver_arr2d.append(tmp_arr)
-	return ver_arr2d
-
-
-
-
+# For determining which object to draw
+object_to_draw = "SPHERE"
 
 # A general OpenGL initialization function.  Sets all of the initial parameters.
 def InitGL(Width, Height):				# We call this right after our OpenGL window is created.
@@ -59,7 +41,6 @@ def InitGL(Width, Height):				# We call this right after our OpenGL window is cr
 
 	glMatrixMode(GL_MODELVIEW)
 
-
 # The function called when our window is resized (which shouldn't happen if you enable fullscreen, below)
 def ReSizeGLScene(Width, Height):
 	if Height == 0:						# Prevent A Divide By Zero If The Window Is Too Small
@@ -71,7 +52,6 @@ def ReSizeGLScene(Width, Height):
 	gluPerspective(45.0, float(Width)/float(Height), 0.1, 100.0)
 	glMatrixMode(GL_MODELVIEW)
 
-
 # The main drawing function.
 def DrawGLScene():
 	global r_angle
@@ -79,100 +59,88 @@ def DrawGLScene():
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);	# Clear The Screen And The Depth Buffer
 	glLoadIdentity();				# Reset The View
 	
-	glTranslatef(0.0,0.0,-6.0);	# Move Left And Into The Screen
+	glTranslatef(0.0,0.0,-6.0);	# Move Into The Screen
 
-	glRotatef(r_angle,1.0,1.0,0.0);	# Rotate The Pyramid On It's Y Axis
+	glRotatef(r_angle,1.0,1.0,0.0);	# Rotate The Object On It's X and Y Axis
+
+	colors = ((1,0,0),(0,1,0),(0,0,1),(0.3,0.3,0.3))
 
 	if object_to_draw == "SPHERE":
-		sp_ver_arr = generate_sphere_vertices(1, sd, sd)
-		for i in range(sd):
-			for j in range(sd):
-				
-				glBegin(GL_QUADS);			# Start Drawing The Pyramid
-
-				glColor3f(1, 0, 0);		# Red
-				glVertex3f(sp_ver_arr[i][j].x,sp_ver_arr[i][j].y,sp_ver_arr[i][j].z);		# stack_step = i, sector_step=j
-
-				glColor3f(0, 1, 0);		# Green
-				glVertex3f(sp_ver_arr[i+1][j].x,sp_ver_arr[i+1][j].y,sp_ver_arr[i+1][j].z);		# stack_step = i+1, sector_step=j
-
-				glColor3f(0, 0, 1)		# Blue
-				glVertex3f(sp_ver_arr[i+1][j+1].x,sp_ver_arr[i+1][j+1].y,sp_ver_arr[i+1][j+1].z);		# stack_step = i+1, sector_step=j+1
-
-				glColor3f(1, 1, 0)		# Yellow
-				glVertex3f(sp_ver_arr[i][j+1].x,sp_ver_arr[i][j+1].y,sp_ver_arr[i][j+1].z);		# stack_step = i+1, sector_step=j+1
-
-				glEnd();
-
-		sphere_new = Obj3d.sphere(1, sd, sd)
-		glBegin(GL_QUADS)
-
-		for poly in sphere_new.poly_arr:
-			glColor3f(1, 0, 0)
-			glVertex3f(poly[0].x,poly[0].y,poly[0].z)
-		
-			glColor3f(0, 1, 0)
-			glVertex3f(poly[1].x,poly[1].y,poly[1].z)
-
-			glColor3f(0, 0, 1)
-			glVertex3f(poly[2].x,poly[2].y,poly[2].z)
-
-			glColor3f(0, 1, 1)
-			glVertex3f(poly[3].x,poly[3].y,poly[3].z)
-
+		sphere = Obj3d.sphere(2, sd, sd)		# Create a new sphere
+		glBegin(GL_QUADS)		# Begin to draw quads
+		# Sphere object consists of polygons
+		# We have to iterate them and draw them
+		for poly in sphere.poly_arr:
+			# Iterate vertices of the polygon
+			for j in range(len(poly)):
+				glColor3f(colors[j][0], colors[j][1], colors[j][2])		# Change color of each vertice of a polygon
+				glVertex3f(poly[j].x,poly[j].y,poly[j].z)
 		glEnd()
 
 	elif object_to_draw == "CYLINDER":
-		cylinder = Obj3d.cylinder(1, 3, sd)
-		colors = ((1,0,0),(0,1,0),(0,0,1))
+		cylinder = Obj3d.cylinder(1, 2, sd)		# Create a cylinder object
+		# Cylinder object consists of polygons (circles at the bottom and at the top are single polygons)
+		# We have to iterate them and draw them
 		for poly in cylinder.poly_arr:
-			glBegin(GL_POLYGON)
+			glBegin(GL_POLYGON)		# Begin to draw polygons
+			# Iterate vertices of the polygon
 			for i in range(len(poly)):
-				glColor3f(colors[i%3][0], colors[i%3][1], colors[i%3][2])
+				glColor3f(colors[i%3][0], colors[i%3][1], colors[i%3][2])		# Change color of each vertice of a polygon
 				glVertex3f(poly[i].x,poly[i].y,poly[i].z)
 			glEnd()
 
 	elif object_to_draw == "CUBE":
-		cube = []
-		a = 2
+		cube = []		# Cube array consists of 6 square object
+		a = 2		# a is the edge length
+		# Iterate for each surface
 		for i in range(6):
-			cube.append(Obj3d.square(a, sd))
+			cube.append(Obj3d.square(a, sd))		# Add the surface to the cube array
 			
+		# Transformations for surface closest to screen
 		cube[0].translate(0, 0, a/2)
 
+		# Transformations for surface farthest to screen
 		cube[1].rotate("y", Vec3d(0, 0, 0), 180)
 		cube[1].translate(0, 0, a/2)
 
+		# Transformations for the right surface
 		cube[2].rotate("y", Vec3d(0, 0, 0), 90)
 		cube[2].translate(0, 0, a/2)
 
+		# Transformations for the left surface
 		cube[3].rotate("y", Vec3d(0, 0, 0), -90)
 		cube[3].translate(0, 0, a/2)
 
+		# Transformations for the bottom surface
 		cube[4].rotate("x", Vec3d(0, 0, 0), 90)
 		cube[4].translate(0, 0, a/2)
 
+		# Transformations for the top surface
 		cube[5].rotate("x", Vec3d(0, 0, 0), -90)
 		cube[5].translate(0, 0, a/2)
-			
+		
+		# Iterate each square in the cube to draw them
 		for square in cube:
-			vertice_arr = square.transform()
+			vertice_arr = square.transform()		# Vertice array of all polygons of the square
+			# For each 4 vertices of a polygon
 			for i in range(len(vertice_arr) // 4):
-				glBegin(GL_POLYGON)
+				glBegin(GL_QUADS)
+				
 				glColor3f(1,0,0)
-				glVertex3f(vertice_arr[i*4].x,vertice_arr[i*4].y,vertice_arr[i*4].z)
+				glVertex3f(vertice_arr[i*4].x,vertice_arr[i*4].y,vertice_arr[i*4].z)		# Left top vertice
 				
 				glColor3f(0,1,0)
-				glVertex3f(vertice_arr[i*4+1].x,vertice_arr[i*4+1].y,vertice_arr[i*4+1].z)
+				glVertex3f(vertice_arr[i*4+1].x,vertice_arr[i*4+1].y,vertice_arr[i*4+1].z)		# Right top vertice
 				
 				glColor3f(0,0,1)
-				glVertex3f(vertice_arr[i*4+2].x,vertice_arr[i*4+2].y,vertice_arr[i*4+2].z)
+				glVertex3f(vertice_arr[i*4+2].x,vertice_arr[i*4+2].y,vertice_arr[i*4+2].z)		# Right bottom vertice
 				
 				glColor3f(0.5,0.5,0.5)
-				glVertex3f(vertice_arr[i*4+3].x,vertice_arr[i*4+3].y,vertice_arr[i*4+3].z)
+				glVertex3f(vertice_arr[i*4+3].x,vertice_arr[i*4+3].y,vertice_arr[i*4+3].z)		# Left bottom vertice
+				
 				glEnd()
 
-	r_angle  = r_angle + 1				# Increase The Rotation Variable For The Triangle
+	r_angle  = r_angle + 1				# Increase The Rotation Variable For The Object
 
 	#  since this is double buffered, swap the buffers to display what just got drawn.
 	glutSwapBuffers()
@@ -190,21 +158,24 @@ def keyPressed(key, x, y):
 		glutLeaveMainLoop()
 		return
 	elif key_ascii == 49:
+		# Key : 1
 		object_to_draw = "CUBE"
 		
 	elif key_ascii == 50:
+		# Key : 2
 		object_to_draw = "SPHERE"
 
 	elif key_ascii == 51:
+		# Key : 3
 		object_to_draw = "CYLINDER"
 
 	elif key_ascii == 43:
+		# Key : +
 		sd += 1
 
 	elif key_ascii == 45:
+		# Key : -
 		sd -= 1
-
-
 
 def main():
 	global window
@@ -252,6 +223,9 @@ def main():
 	glutMainLoop()
 
 # Print message to console, and kick off the main to get it rolling.
-print ("Hit ESC key to quit.")
+print ("Press 1 for CUBE")
+print ("Press 2 for SPHERE")
+print ("Press 3 for CYLINDER")
+print ("Press ESC to quit")
 main()
 
