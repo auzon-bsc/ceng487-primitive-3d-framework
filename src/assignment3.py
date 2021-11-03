e9f007b1e7b06cb05a25a1c78c21e26f81f97d40
Oğuzhan Özer <oguzhanozer90@outlook.com>
2021-11-03 18:27:47 +0300

    working without comments

--- src/assignment3.py
+++ src/assignment3.py
@@ -8,6 +8,7 @@ from OpenGL.GLUT import *
 from OpenGL.GLU import *
 import sys
 import math
+from obj3d import Obj3d
 
 from vec3d import Vec3d
 
@@ -15,15 +16,12 @@ from vec3d import Vec3d
 window = 0
 
 # Rotation angle for the triangle.
-rtri = 0.0
+r_angle = 0.0
 
-# Rotation angle for the quadrilateral.
-rquad = 0.0
+# Subdivision number
+sd = 10
 
-# Subdivision numbers
-sd_cube = 10
-sd_sphere = 10
-sd_cylinder = 10
+object_to_draw = "NONE"
 
 def generate_sphere_vertices(radius, sector_count, stack_count):
 	ver_arr2d = []
@@ -41,6 +39,10 @@ def generate_sphere_vertices(radius, sector_count, stack_count):
 			tmp_arr.append(tmp_vec)
 		ver_arr2d.append(tmp_arr)
 	return ver_arr2d
+
+
+
+
 
 # A general OpenGL initialization function.  Sets all of the initial parameters.
 def InitGL(Width, Height):				# We call this right after our OpenGL window is created.
@@ -72,90 +74,105 @@ def ReSizeGLScene(Width, Height):
 
 # The main drawing function.
 def DrawGLScene():
-	global rtri, rquad
+	global r_angle
 
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);	# Clear The Screen And The Depth Buffer
 	glLoadIdentity();				# Reset The View
-	glTranslatef(-1.5,0.0,-6.0);	# Move Left And Into The Screen
+	
+	glTranslatef(0.0,0.0,-6.0);	# Move Left And Into The Screen
 
-	glRotatef(rtri,0.0,1.0,0.0);	# Rotate The Pyramid On It's Y Axis
+	glRotatef(r_angle,1.0,1.0,0.0);	# Rotate The Pyramid On It's Y Axis
 
-	# glBegin(GL_POLYGON);			# Start Drawing The Pyramid
+	if object_to_draw == "SPHERE":
+		sp_ver_arr = generate_sphere_vertices(1, sd, sd)
+		for i in range(sd):
+			for j in range(sd):
+				
+				glBegin(GL_QUADS);			# Start Drawing The Pyramid
 
-	# glColor3f(1.0,0.0,0.0);			# Red
-	# glVertex3f(0.0,1.0,0.0);		# Top Of Triangle (Front)
+				glColor3f(1, 0, 0);		# Red
+				glVertex3f(sp_ver_arr[i][j].x,sp_ver_arr[i][j].y,sp_ver_arr[i][j].z);		# stack_step = i, sector_step=j
 
-	sp_ver_arr = generate_sphere_vertices(1, sd_sphere, sd_sphere)
-	for i in range(sd_sphere):
-		for j in range(sd_sphere):
-			
-			glBegin(GL_POLYGON);			# Start Drawing The Pyramid
+				glColor3f(0, 1, 0);		# Green
+				glVertex3f(sp_ver_arr[i+1][j].x,sp_ver_arr[i+1][j].y,sp_ver_arr[i+1][j].z);		# stack_step = i+1, sector_step=j
 
-			glColor3f(1, 0, 0);		# Red
-			glVertex3f(sp_ver_arr[i][j].x,sp_ver_arr[i][j].y,sp_ver_arr[i][j].z);		# stack_step = i, sector_step=j
+				glColor3f(0, 0, 1)		# Blue
+				glVertex3f(sp_ver_arr[i+1][j+1].x,sp_ver_arr[i+1][j+1].y,sp_ver_arr[i+1][j+1].z);		# stack_step = i+1, sector_step=j+1
 
-			glColor3f(0, 1, 0);		# Green
-			glVertex3f(sp_ver_arr[i+1][j].x,sp_ver_arr[i+1][j].y,sp_ver_arr[i+1][j].z);		# stack_step = i+1, sector_step=j
+				glColor3f(1, 1, 0)		# Yellow
+				glVertex3f(sp_ver_arr[i][j+1].x,sp_ver_arr[i][j+1].y,sp_ver_arr[i][j+1].z);		# stack_step = i+1, sector_step=j+1
 
-			glColor3f(0, 0, 1)		# Blue
-			glVertex3f(sp_ver_arr[i+1][j+1].x,sp_ver_arr[i+1][j+1].y,sp_ver_arr[i+1][j+1].z);		# stack_step = i+1, sector_step=j+1
+				glEnd();
 
-			glColor3f(1, 1, 0)		# Yellow
-			glVertex3f(sp_ver_arr[i][j+1].x,sp_ver_arr[i][j+1].y,sp_ver_arr[i][j+1].z);		# stack_step = i+1, sector_step=j+1
+		sphere_new = Obj3d.sphere(1, sd, sd)
+		glBegin(GL_QUADS)
 
-			glEnd();
-
-	# glEnd();
+		for poly in sphere_new.poly_arr:
+			glColor3f(1, 0, 0)
+			glVertex3f(poly[0].x,poly[0].y,poly[0].z)
 		
-	
-	
+			glColor3f(0, 1, 0)
+			glVertex3f(poly[1].x,poly[1].y,poly[1].z)
 
+			glColor3f(0, 0, 1)
+			glVertex3f(poly[2].x,poly[2].y,poly[2].z)
 
-	glLoadIdentity();
-	glTranslatef(1.5,0.0,-7.0);		# Move Right And Into The Screen
-	glRotatef(rquad,1.0,1.0,1.0);	# Rotate The Cube On X, Y & Z
-	glBegin(GL_QUADS);				# Start Drawing The Cube
+			glColor3f(0, 1, 1)
+			glVertex3f(poly[3].x,poly[3].y,poly[3].z)
 
+		glEnd()
 
-	glColor3f(0.0,1.0,0.0);			# Set The Color To Blue
-	glVertex3f( 1.0, 1.0,-1.0);		# Top Right Of The Quad (Top)
-	glVertex3f(-1.0, 1.0,-1.0);		# Top Left Of The Quad (Top)
-	glVertex3f(-1.0, 1.0, 1.0);		# Bottom Left Of The Quad (Top)
-	glVertex3f( 1.0, 1.0, 1.0);		# Bottom Right Of The Quad (Top)
+	elif object_to_draw == "CYLINDER":
+		cylinder = Obj3d.cylinder(1, 3, sd)
+		colors = ((1,0,0),(0,1,0),(0,0,1))
+		for poly in cylinder.poly_arr:
+			glBegin(GL_POLYGON)
+			for i in range(len(poly)):
+				glColor3f(colors[i%3][0], colors[i%3][1], colors[i%3][2])
+				glVertex3f(poly[i].x,poly[i].y,poly[i].z)
+			glEnd()
 
-	glColor3f(1.0,0.5,0.0);			# Set The Color To Orange
-	glVertex3f( 1.0,-1.0, 1.0);		# Top Right Of The Quad (Bottom)
-	glVertex3f(-1.0,-1.0, 1.0);		# Top Left Of The Quad (Bottom)
-	glVertex3f(-1.0,-1.0,-1.0);		# Bottom Left Of The Quad (Bottom)
-	glVertex3f( 1.0,-1.0,-1.0);		# Bottom Right Of The Quad (Bottom)
+	elif object_to_draw == "CUBE":
+		cube = []
+		a = 2
+		for i in range(6):
+			cube.append(Obj3d.square(a, sd))
+			
+		cube[0].translate(0, 0, a/2)
 
-	glColor3f(1.0,0.0,0.0);			# Set The Color To Red
-	glVertex3f( 1.0, 1.0, 1.0);		# Top Right Of The Quad (Front)
-	glVertex3f(-1.0, 1.0, 1.0);		# Top Left Of The Quad (Front)
-	glVertex3f(-1.0,-1.0, 1.0);		# Bottom Left Of The Quad (Front)
-	glVertex3f( 1.0,-1.0, 1.0);		# Bottom Right Of The Quad (Front)
+		cube[1].rotate("y", Vec3d(0, 0, 0), 180)
+		cube[1].translate(0, 0, a/2)
 
-	glColor3f(1.0,1.0,0.0);			# Set The Color To Yellow
-	glVertex3f( 1.0,-1.0,-1.0);		# Bottom Left Of The Quad (Back)
-	glVertex3f(-1.0,-1.0,-1.0);		# Bottom Right Of The Quad (Back)
-	glVertex3f(-1.0, 1.0,-1.0);		# Top Right Of The Quad (Back)
-	glVertex3f( 1.0, 1.0,-1.0);		# Top Left Of The Quad (Back)
+		cube[2].rotate("y", Vec3d(0, 0, 0), 90)
+		cube[2].translate(0, 0, a/2)
 
-	glColor3f(0.0,0.0,1.0);			# Set The Color To Blue
-	glVertex3f(-1.0, 1.0, 1.0);		# Top Right Of The Quad (Left)
-	glVertex3f(-1.0, 1.0,-1.0);		# Top Left Of The Quad (Left)
-	glVertex3f(-1.0,-1.0,-1.0);		# Bottom Left Of The Quad (Left)
-	glVertex3f(-1.0,-1.0, 1.0);		# Bottom Right Of The Quad (Left)
+		cube[3].rotate("y", Vec3d(0, 0, 0), -90)
+		cube[3].translate(0, 0, a/2)
 
-	glColor3f(1.0,0.0,1.0);			# Set The Color To Violet
-	glVertex3f( 1.0, 1.0,-1.0);		# Top Right Of The Quad (Right)
-	glVertex3f( 1.0, 1.0, 1.0);		# Top Left Of The Quad (Right)
-	glVertex3f( 1.0,-1.0, 1.0);		# Bottom Left Of The Quad (Right)
-	glVertex3f( 1.0,-1.0,-1.0);		# Bottom Right Of The Quad (Right)
-	glEnd();						# Done Drawing The Quad
+		cube[4].rotate("x", Vec3d(0, 0, 0), 90)
+		cube[4].translate(0, 0, a/2)
 
-	rtri  = rtri + 0.01				# Increase The Rotation Variable For The Triangle
-	rquad = rquad - 0.01			# Decrease The Rotation Variable For The Quad
+		cube[5].rotate("x", Vec3d(0, 0, 0), -90)
+		cube[5].translate(0, 0, a/2)
+			
+		for square in cube:
+			vertice_arr = square.transform()
+			for i in range(len(vertice_arr) // 4):
+				glBegin(GL_POLYGON)
+				glColor3f(1,0,0)
+				glVertex3f(vertice_arr[i*4].x,vertice_arr[i*4].y,vertice_arr[i*4].z)
+				
+				glColor3f(0,1,0)
+				glVertex3f(vertice_arr[i*4+1].x,vertice_arr[i*4+1].y,vertice_arr[i*4+1].z)
+				
+				glColor3f(0,0,1)
+				glVertex3f(vertice_arr[i*4+2].x,vertice_arr[i*4+2].y,vertice_arr[i*4+2].z)
+				
+				glColor3f(0.5,0.5,0.5)
+				glVertex3f(vertice_arr[i*4+3].x,vertice_arr[i*4+3].y,vertice_arr[i*4+3].z)
+				glEnd()
+
+	r_angle  = r_angle + 1				# Increase The Rotation Variable For The Triangle
 
 	#  since this is double buffered, swap the buffers to display what just got drawn.
 	glutSwapBuffers()
@@ -163,6 +180,8 @@ def DrawGLScene():
 
 # The function called whenever a key is pressed. Note the use of Python tuples to pass in: (key, x, y)
 def keyPressed(key, x, y):
+	global object_to_draw
+	global sd
 	# If escape is pressed, kill everything.
 	# ord() is needed to get the keycode
 	key_ascii = ord(key)
@@ -171,20 +190,20 @@ def keyPressed(key, x, y):
 		glutLeaveMainLoop()
 		return
 	elif key_ascii == 49:
-		# 1 -> Cube
-		pass
+		object_to_draw = "CUBE"
+		
 	elif key_ascii == 50:
-		# 2 -> Sphere
-		pass
+		object_to_draw = "SPHERE"
+
 	elif key_ascii == 51:
-		# 3 -> Cylinder
-		pass
+		object_to_draw = "CYLINDER"
+
 	elif key_ascii == 43:
-		# + -> Increase subdivisions
-		pass
+		sd += 1
+
 	elif key_ascii == 45:
-		# - -> Decrease subdivisions
-		pass
+		sd -= 1
+
 
 
 def main():
--- src/obj3d.py
+++ src/obj3d.py
@@ -4,9 +4,11 @@
 # October 2021
 
 from logging import error
+import math
+from sys import api_version
 from mat3d import Mat3d
 from enum import Enum
-
+import copy
 from vec3d import Vec3d
 
 class TransformationOrder(Enum):
@@ -22,16 +24,13 @@ class Obj3d:
     Args:
         point_arr (list): Point array
     """
-  def __init__(self, point_arr) -> None:
+  def __init__(self, poly_arr) -> None:
     
-    self.ver_arr = []
     self.transformation = Mat3d.identity()
     self.translation = Mat3d.identity()
     self.rotation = Mat3d.identity()
     self.scaling = Mat3d.identity()
-    for point in point_arr:
-      vertice = Vec3d(point[0], point[1], point[2], point[3])
-      self.ver_arr.append(vertice)
+    self.poly_arr = copy.deepcopy(poly_arr)
     self.stack = [Mat3d.identity()]
 
   def push_transformation(self):
@@ -116,14 +115,136 @@ class Obj3d:
           raise error("There is a problem with TransformationOrder ENUM")
 
     tmp_ver_arr = []
-    for i in range(len(self.ver_arr)):
-      tmp_mat = self.transformation.matrix.multiply(self.ver_arr[i].matrix)
-      tmp_x = tmp_mat.matrix_arr[0][0]
-      tmp_y = tmp_mat.matrix_arr[1][0]
-      tmp_z = tmp_mat.matrix_arr[2][0]
-      tmp_w = tmp_mat.matrix_arr[3][0]
-      tmp_ver = Vec3d(tmp_x, tmp_y, tmp_z, tmp_w)
-      tmp_ver_arr.append(tmp_ver)
+    for i in range(len(self.poly_arr)):
+      for j in range(len(self.poly_arr[i])):
+
+        tmp_mat = self.transformation.matrix.multiply(self.poly_arr[i][j].matrix)
+        tmp_x = tmp_mat.matrix_arr[0][0]
+        tmp_y = tmp_mat.matrix_arr[1][0]
+        tmp_z = tmp_mat.matrix_arr[2][0]
+        tmp_w = tmp_mat.matrix_arr[3][0]
+        tmp_ver = Vec3d(tmp_x, tmp_y, tmp_z, tmp_w)
+        tmp_ver_arr.append(tmp_ver)
 
     return tmp_ver_arr
+
+  @staticmethod
+  def sphere(radius, sector_count, stack_count):
+    point_arr = []
+    for stack_step in range(stack_count + 1):
+      sector_arr = []
+      for sector_step in range(sector_count + 1):
+        theta = (2 * math.pi) * (sector_step / sector_count)
+        phi = (math.pi / 2) - (math.pi * stack_step / stack_count)
+
+        x = radius * math.cos(phi) * math.cos(theta)
+        y = radius * math.cos(phi) * math.sin(theta)
+        z = radius * math.sin(phi)
+        w = 1
+
+        tmp_vec = Vec3d(x, y, z, w)
+        sector_arr.append(tmp_vec)
+      point_arr.append(sector_arr)
+
+    poly_arr = []
+    for i in range(sector_count):
+      for j in range(stack_count):
+        tmp_arr = []
+        tmp_arr.append(point_arr[i][j].clone())
+        tmp_arr.append(point_arr[i+1][j].clone())
+        tmp_arr.append(point_arr[i+1][j+1].clone())
+        tmp_arr.append(point_arr[i][j+1].clone())
+        poly_arr.append(tmp_arr)
+
+    return Obj3d(poly_arr)
+
+  @staticmethod
+  def cylinder(radius, height, sector_count):
+    top_points = []
+    bot_points = []
+
+    for sector_step in range(sector_count + 1):
+      theta = (2 * math.pi) * (sector_step / sector_count)
+      x = radius * math.cos(theta)
+      z = radius * math.sin(theta)
+      y = height / 2
+      w = 1
+      top_points.append(Vec3d(x, y, z, w))
+      bot_points.append(Vec3d(x, -y, z, w))
+
+    poly_arr = []
+    
+    poly_arr.append(top_points)
+
+    for i in range(sector_count):
+      quad_points = []
+      quad_points.append(top_points[i].clone())
+      quad_points.append(bot_points[i].clone())
+      quad_points.append(bot_points[i+1].clone())
+      quad_points.append(top_points[i+1].clone())
+      poly_arr.append(quad_points)
+    
+    poly_arr.append(bot_points)
+
+    return Obj3d(poly_arr)
+    
+  @staticmethod
+  def square(a, sector_count):
+    poly_arr = []   # array of polygons/vertice arrays
+    pivot_vertice = Vec3d(-(a/2),(a/2),0,1)
+    point_arr = []
+    for i in range(sector_count + 1):
+      tmp_arr = []
+      
+      offset_y = a * i / sector_count   # offset_y
+      for j in range(sector_count + 1):
+        offset_x = a * j / sector_count   # offset_x
+        tmp_v = pivot_vertice.clone()
+        tmp_v.x += offset_x
+        tmp_v.y -= offset_y
+        tmp_arr.append(tmp_v)
+      point_arr.append(tmp_arr)       
+    
+    for i in range(sector_count):
+      for j in range(sector_count):
+        tmp_arr = []
+        tmp_arr.append(point_arr[i][j])
+        tmp_arr.append(point_arr[i][j+1])
+        tmp_arr.append(point_arr[i+1][j+1])
+        tmp_arr.append(point_arr[i+1][j])
+        poly_arr.append(tmp_arr)
+
+    s1 = Obj3d(copy.deepcopy(poly_arr))   # Surface 1
+    s2 = Obj3d(copy.deepcopy(poly_arr))
+    s3 = Obj3d(copy.deepcopy(poly_arr))
+    s4 = Obj3d(copy.deepcopy(poly_arr))
+    s5 = Obj3d(copy.deepcopy(poly_arr))
+    s6 = Obj3d(copy.deepcopy(poly_arr))
+
+    s1.translate(0, 0, a/2)
+  
+    s2.translate(0, 0, -a/2)
+    
+    s3.rotate("y", Vec3d(0, 0, 0), 90)
+    s3.translate(a/2, 0, 0)
+
+    s4.rotate("y", Vec3d(0, 0, 0), -90)
+    s4.translate(-a/2, 0, 0)
+
+    s5.rotate("x", Vec3d(0, 0, 0), 90)
+    s5.translate(0, a/2, 0)
+    
+    s6.rotate("x", Vec3d(0, 0, 0), -90)
+    s6.translate(0, -a/2, 0)
+    
+    
+    # arr1 = s1.transform()
+    # arr2 = s2.transform()
+    # arr3 = s3.transform()
+    # arr4 = s4.transform()
+    # arr5 = s5.transform()
+    # arr6 = s6.transform()
+
+    return Obj3d(poly_arr)
+
 
